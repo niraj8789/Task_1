d94c317d587158c0361664af0ae245cc864ee001
Neeraj Kumar <88700951+niraj8789@users.noreply.github.com>
2023-03-23 11:23:39 +0530

    Add files via upload

---
+++ Task_1.java
@@ -0,0 +1,15 @@
+public class Server {
+    private String id;
+    private String name;
+    private String language;
+    private String framework;
+
+    public Server(String id, String name, String language, String framework) {
+        this.id = id;
+        this.name = name;
+        this.language = language;
+        this.framework = framework;
+    }
+
+    // getters and setters
+}
